2b718c74152008ac4c12315a1bf17a810eebc68b
Luiza Sirianni <80859622+luizasirianni@users.noreply.github.com>
2023-03-17 14:45:00 +0000

    Criado teste com Selenium para verificar presença do produto 'Belt'

---
+++ test/web/test_selenium_ide.py
@@ -0,0 +1,32 @@
+import pytest
+import time
+import json
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+class TestConsultarBelt():
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5) #espera de 5 segundos para os elementos da pagina carregarem
+        self.driver.maximize_window()
+        self.vars = {}
+
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_consultarBelt(self):
+        self.driver.get('http://demostore.supersqa.com/')
+        #self.driver.set_window_size(1280, 680)
+        self.driver.find_element(By.ID, 'woocommerce-product-search-field-0')
+        self.driver.find_element(By.ID, 'woocommerce-product-search-field-0').send_keys("Belt")
+        self.driver.find_element(By.ID, 'woocommerce-product-search-field-0').send_keys(Keys.ENTER)
+        assert self.driver.find_element(By.XPATH, '//*[@id="product-17"]/div[2]/h1').text == "Belt"
+        assert self.driver.find_element(By.XPATH, '//*[@id="product-17"]/div[2]/p/span/bdi').text == "$37.14"
+
