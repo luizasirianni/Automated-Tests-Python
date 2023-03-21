b27139c1fbec0e7ee4510f3107118a366baf5840
Luiza Sirianni <80859622+luizasirianni@users.noreply.github.com>
2023-03-21 16:02:05 +0000

    Tratado sobreposição de prints

--- test/web/test_selenium_ide.py
+++ test/web/test_selenium_ide.py
@@ -21,18 +21,18 @@ class TestConsultarBelt():
     def teardown_method(self, method):
         self.driver.quit()
 
-    @pytest.mark.parametrize('termo, produto, preco, xpathText, xpathPrice',[
-        ('belt', 'Belt', '$37.14', '//*[@id="product-17"]/div[2]/h1', '//*[@id="product-17"]/div[2]/p/span/bdi'),
-        ('album', 'Album', '$15.00', '//*[@id="product-24"]/div[2]/h1', '//*[@id="product-24"]/div[2]/p/span/bdi'),
-        ('beanie', 'Beanie', '$18.00', '//*[@id="main"]/ul/li[2]/a[1]/h2','//*[@id="main"]/ul/li[2]/a[1]/span[2]/ins/span/bdi')
+    @pytest.mark.parametrize('id, termo, produto, preco, xpathText, xpathPrice',[
+        ('1','belt', 'Belt', '$37.14', '//*[@id="product-17"]/div[2]/h1', '//*[@id="product-17"]/div[2]/p/span/bdi'),
+        ('2','album', 'Album', '$15.00', '//*[@id="product-24"]/div[2]/h1', '//*[@id="product-24"]/div[2]/p/span/bdi'),
+        ('3','beanie', 'Beanie', '$18.00', '//*[@id="main"]/ul/li[2]/a[1]/h2','//*[@id="main"]/ul/li[2]/a[1]/span[2]/ins/span/bdi')
     ])
-    def test_consultar_produto_Enter(self, termo, produto, preco, xpathText, xpathPrice):
+    def test_consultar_produto_Enter(self, id, termo, produto, preco, xpathText, xpathPrice):
         pasta_prints = 'D:/python/Automated-Tests-Python/test/prints/'
         self.driver.get('http://demostore.supersqa.com/')
-        self.driver.get_screenshot_as_file(f'{pasta_prints}homepage.png')
+        self.driver.get_screenshot_as_file(f'{pasta_prints}teste-{id}-homepage.png')
         self.driver.find_element(By.ID, 'woocommerce-product-search-field-0')
         self.driver.find_element(By.ID, 'woocommerce-product-search-field-0').send_keys(termo)
-        self.driver.get_screenshot_as_file(f'{pasta_prints}campodepesquisa.png')
+        self.driver.get_screenshot_as_file(f'{pasta_prints}teste-{id}-campodepesquisa.png')
         self.driver.find_element(By.ID, 'woocommerce-product-search-field-0').send_keys(Keys.ENTER)
         assert self.driver.find_element(By.XPATH, xpathText).text == produto
         assert self.driver.find_element(By.XPATH, xpathPrice).text == preco
